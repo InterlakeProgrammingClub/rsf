5bb398feb426fcd31490d08da2ddf1ab786a929f
aarnavpbhat <aarnavpbhat@gmail.com>
2024-11-30 22:22:43 -0800

    test carousel

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,54 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
 import { motion } from "framer-motion";
-import { useInView } from "react-intersection-observer";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import headerimg from "../assets/headerimg.jpg";
+import officer1 from "../assets/officer1.jpg";
+import officer2 from "../assets/officer2.jpg";
+import officer3 from "../assets/officer3.jpg";
+import officer4 from "../assets/officer4.jpg";
+import officer5 from "../assets/officer5.jpg";
+import officer6 from "../assets/officer6.jpg";
 import data from "../data.json";
 
 const About = () => {
-  const [expanded, setExpanded] = useState(null);
+  const leadershipImages = [officer1, officer2, officer3, officer4, officer5, officer6];
 
-  const handleToggle = (index) => {
-    setExpanded(expanded === index ? null : index);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
-
-  const meetings = [
-    {
-      date: "#/#/#",
-      details: (
-        <>
-          <p>Talk about what you did in the meeting</p>
-        </>
-      ),
-      image: headerimg
-    },
-    {
-      date: "#/#/#",
-      details: (
-        <>
-          <p>Talk about what you did in the meeting</p>
-        </>
-      ),
-      image: headerimg
-    },
-    {
-      date: "#/#/#",
-      details: (
-        <>
-          <p>Talk about what you did in the meeting</p>
-        </>
-      ),
-      image: headerimg
-    },
-    {
-      date: "#/#/#",
-      details: (
-        <>
-          <p>Talk about what you did in the meeting</p>
-        </>
-      ),
-      image: headerimg
-    }
-  ];
 
   return (
     <div className="text-center">
@@ -64,7 +54,7 @@ const About = () => {
           <motion.div
             whileHover={{
               scale: 1.05,
-              transition: { duration: 0.2, ease: "easeInOut" }
+              transition: { duration: 0.2, ease: "easeInOut" },
             }}
             className="mb-4 inline-block"
           >
@@ -77,98 +67,51 @@ const About = () => {
           </motion.div>
         </div>
       </motion.div>
+
       <div className="container mx-auto px-4 py-8">
         <h2 className="mb-4 text-2xl font-bold underline underline-offset-2">
           Our Leadership Team:
         </h2>
-        <div className="mb-8 flex flex-col items-center space-y-4">
-          {Object.keys(data.leadership).map((role) => (
-            <>
-              {Array.isArray(data.leadership[role]) ? (
-                data.leadership[role].map((name, i) => (
-                  <p className="text-lg font-bold" key={i}>
-                    {role}: {name}
-                  </p>
-                ))
-              ) : (
-                <p className="text-lg font-bold">
-                  {role}: {data.leadership[role]}
-                </p>
-              )}
-            </>
-          ))}
+
+        <div className="carousel-container">
+          <Slider {...settings}>
+            {leadershipImages.map((image, index) => (
+              <div key={index} className="carousel-slide">
+                <div className="w-full h-full flex justify-center pb-8">
+                  <img
+                    src={image}
+                    alt={`Leadership Team ${index + 1}`}
+                    className="w-full h-full object-cover rounded-md shadow-lg"
+                  />
+                </div>
+              </div>
+            ))}
+          </Slider>
         </div>
+
         <div className="mb-4 flex flex-col items-center space-y-4">
-          <h2 className="text-2xl font-bold underline underline-offset-2">
+          <h2 className="text-2xl font-bold underline underline-offset-2 pt-8">
             What we do:
           </h2>
           <p className="text-lg">{data.description}</p>
         </div>
+
         <div className="mb-4 flex flex-col items-center space-y-4">
           <h2 className="text-2xl font-bold underline underline-offset-2">
             When we meet:
           </h2>
           <p className="text-lg">
-            We meet every {data.time} in {data.location}
+            {data.time} at {data.location}
           </p>
         </div>
-      </div>
-      <div className="mx-auto max-w-4xl px-4 pb-10 pt-4 text-[#a562db]">
-        <div className="grid grid-cols-1 gap-8 pt-4 md:grid-cols-2">
-          {meetings.map((meeting, index) => (
-            <MeetingOption
-              key={index}
-              index={index}
-              date={meeting.date}
-              details={meeting.details}
-              image={meeting.image}
-              expanded={expanded}
-              handleToggle={handleToggle}
-            />
-          ))}
+
+        <div className="mb-4 flex flex-col items-center space-y-4">
+          <h2 className="text-2xl font-bold underline underline-offset-2">
+            Past Meetings:
+          </h2>
+          <p className="text-lg">Past Meetings Available in Discord!</p>
         </div>
       </div>
-    </div>
-  );
-};
-
-const MeetingOption = ({
-  index,
-  date,
-  details,
-  image,
-  expanded,
-  handleToggle
-}) => {
-  const { ref: inViewRef, inView } = useInView({
-    triggerOnce: true,
-    threshold: 0.2
-  });
-
-  return (
-    <div ref={inViewRef} className="w-full">
-      <motion.div
-        className="flex h-40 w-full cursor-pointer flex-col items-center justify-center rounded-md bg-[#a562db] bg-opacity-25 p-4 text-center transition duration-300 hover:bg-opacity-50"
-        style={{
-          backgroundImage: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${image})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center"
-        }}
-        initial={{ opacity: 0, y: 50 }}
-        animate={inView ? { opacity: 1, y: 0 } : {}}
-        transition={{ duration: 0.7, ease: "easeInOut" }}
-        whileHover={{
-          scale: 1,
-          boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.3)",
-          transition: { duration: 0.3, ease: "easeInOut" }
-        }}
-        onClick={() => handleToggle(index)}
-      >
-        <h2 className="mb-2 text-3xl font-semibold">{date}</h2>
-      </motion.div>
-      {expanded === index && (
-        <div className="mt-2 rounded-md bg-[#a562db] p-4 text-black">{details}</div>
-      )}
     </div>
   );
 };
